3cf656706509a6890dd001541febe6d7c884a3c6
quang <quanglopxe@gmail.com>
2023-08-24 20:36:23 +0700

    push bt1 bt2

---
+++ DuongThuanQuang/DuongThuanQuang_Bai1/DuongThuanQuang/toaDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuongThuanQuang
+{
+    class toaDo
+    {
+        protected int x, y;
+        protected string ten;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public string Ten
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+        public toaDo()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.ten = null;
+        }
+        public toaDo(int x,int y,string ten)
+        {
+            this.x = x;
+            this.y = y;
+            this.ten = ten;
+        }
+        public void toString()
+        {
+            Console.Write("Toa do {0}({1},{2})", ten, x, y);
+        }
+    }
+}
